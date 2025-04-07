40a46ee3489e94cad3885cc3b6ed28a8c939e0ae
jaider santiago <pjaidersantiago@gmail.com>
2025-04-07 08:20:08 -0500

    creacion de la clase inicio

---
+++ src/main/java/com/mycompany/crud3_mysql/inicio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.crud3_mysql;
+
+/**
+ *
+ * @author SENA
+ */
+public class inicio {
+     public static void main(String[] args) {
+        FormAlumnos objetoFormulario = new FormAlumnos();
+        objetoFormulario.setVisible(true);
+    }
+    
+}
